4108f73aa67eb461b121bd15350935ead1012887
Tomás <tomas950923@gmail.com>
2021-08-25 15:26:44 -0500

    order controller

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -12,27 +12,33 @@ async function show(req, res) {
 }
 
 async function store(req, res) {
-  const user = await User.findOneAndUpdate({ email: req.body.email }, { telephone: req.body.phone, address: req.body.address }, {
-    new: true
-  });
-  if (user) {
-    const order = await Order.create({
-      status: "PENDING",
-      userId: user.id,
-      paymentMethod: req.body.paymentMethod,
-    });
-    if (order) {
-      const products = req.body.items.map((item) => ({
-        orderId: order.id,
-        productId: item,
-        quantity: 1,
-        unit_price: 1,
-      }));
-      const order_product = await OrderProduct.bulkCreate(products);
-      if (order_product) return res.json(order);
+  if (req.headers.authorization === process.env.ULTRA_SECRET_TOKEN) {
+    await User.update(
+      { telephone: req.body.phone, address: req.body.address },
+      { where: { email: req.body.email } }
+    );
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) {
+      const order = await Order.create({
+        status: "PENDING",
+        userId: user.id,
+        paymentMethod: req.body.paymentMethod,
+      });
+      if (order) {
+        const products = req.body.items.map((item) => ({
+          orderId: order.id,
+          productId: item.id,
+          quantity: item.quantity,
+          unit_price: item.unit_price,
+        }));
+        const order_product = await OrderProduct.bulkCreate(products);
+        if (order_product) return res.json(order);
+      }
     }
+    res.sendStatus(403);
+  } else {
+    res.sendStatus(403);
   }
-  res.sendStatus(403);
 }
 
 function update(req, res) {}
