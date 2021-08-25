3534f150ecf93e246e25b637dc1544cd939e697f
iAgis <86081166+iAgis@users.noreply.github.com>
2021-08-25 11:13:22 -0300

    Update privateRoutes.js

--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const privateRoutes = express.Router();
 const dbInitialSetup = require("../dbInitialSetup");
 const checkJwt = require("express-jwt");
+const isAdmin = require("../middlewares/isAdmin");
 
 privateRoutes.use(
   checkJwt({ secret: process.env.TOKEN_SECRET, algorithms: ["HS256"] })
 );
+
+privateRoutes.use(isAdmin);
 
 privateRoutes.patch("/db", async (req, res) => {
   try {
