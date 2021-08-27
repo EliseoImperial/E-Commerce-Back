d669b4ec7be658e8c531565bb9b3fe1a65f04d51
iAgis <86081166+iAgis@users.noreply.github.com>
2021-08-27 16:03:50 -0300

    updateFerrari

--- seeders/ferrariSeeder.js
+++ seeders/ferrariSeeder.js
@@ -5,8 +5,37 @@ module.exports = [
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Ferrari-296GTB.jpeg",
     price: 321400,
+    description: `The 296 GTB, the evolution of Ferrari's mid-engined two-seater sports berlinetta concept, represents a complete revolution for the House of Maranello by including a new 120º V6 engine coupled to a plug-in or plug-in electric motor (PHEV) capable to deliver up to 830 CV. This car once again redefines the idea of ​​driving fun to ensure maximum excitement, not only when seeking maximum performance, but also in everyday use.`,
+    brandId: 2,
+  },
+  {
+    name: "Ferrari SF90 Spider",
+    slug: "Ferrari-SF90-Spider",
+    image:
+      "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/sf90spider.jpg",
+    price: 507300,
     description:
-      "Hace brum, brum y brum... Potencia: muy muy potente. Atrae mujeres, hombres y otros.",
+      "As the Prancing Horse’s first production plug-in hybrid spider, the SF90 Spider sets new performance and innovation benchmarks not only for the marque’s range, but for the entire sports car sector. The new convertible has the same extreme supercar specification and record-breaking performance as the SF90 Stradale yet also adds further driving pleasure and versatility to the mix, thanks to the latest iteration of Ferrari’s signature Retractable Hard Top architecture. This makes the SF90 Spider the ideal car for owners that demand the very pinnacle of Ferrari technology, but still want the thrill and versatility of open-top driving.",
+    brandId: 2,
+  },
+  {
+    name: "Ferrari 290 GBT",
+    slug: "Ferrari-290-GBT",
+    image:
+      "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/ferrari290gbt.jpg",
+    price: 507300,
+    description:
+      "The 296 GTB, an evolution of Ferrari’s mid-rear-engined two-seater sports berlinetta concept, represents a revolution for the Maranello-based company as it introduces the new 120° V6 engine coupled with a plug-in (PHEV) electric motor capable of delivering up to 830 cv. The car thus redefines the idea of driving fun to provide pure excitement not only when pursuing maximum performance but also in everyday driving.",
+    brandId: 2,
+  },
+  {
+    name: "Ferrari Roma",
+    slug: "Ferrari-Roma",
+    image:
+      "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Ferrari-Roma.jpg",
+    price: 507300,
+    description:
+      "The F8 Spider’s greatest achievement is the fact that it unleashes its power instantaneously with zero turbo lag, whilst retaining this V8’s unique and very special soundtrack.",
     brandId: 2,
   },
 ];
