fcb9baa12d0157374cbac59902e9bea234278b21
iAgis <86081166+iAgis@users.noreply.github.com>
2021-08-27 15:39:10 -0300

    Update lamborghiniSeeder.js

--- seeders/lamborghiniSeeder.js
+++ seeders/lamborghiniSeeder.js
@@ -1,57 +1,67 @@
 module.exports = [
   // Lamborghini
   {
-    name: "Lambo Evo Spider",
-    slug: "Lambo-Evo-Spider",
+    name: "Lamborghini Evo Spider",
+    slug: "Lamborghini-Evo-Spider",
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Lambo-Evo-Spider.webp",
-    price: 517826,
-    description: "Luxury I love It C:",
+    price: 257826,
+    description: `The Huracán EVO Spyder has been designed to take the 
+        thrill of the driving experience to the extreme. Colors, aromas and 
+        sounds blend with seductive design and ultra-light materials.
+        Its 640PS V10 throbs and roars furiously, while the
+        supercar tailpipe solo silences everything else. The streamlined, 
+        perfect and streamlined lines cut through the air.`,
     brandId: 3,
   },
   {
-    name: "Lambo Huracan Performante Spyder",
-    slug: "Lambo-Huracan-Performante-Spyder",
+    name: "Lamborghini Huracan Performante Spyder",
+    slug: "Lamborghini-Huracan-Performante-Spyder",
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Lambo-Huracan-Performante-Spyder.jpg",
-    price: 517826,
-    description: "Luxury I love It C:",
+    price: 327525,
+    description:
+      "The Huracán Performante has lowered the roof and has become the highest performing Spyder the Huracán family has ever seen. The design is studied to thrill at first glance and cutting-edge technology to offer unique driving performance. In fact, this newcomer boasts the best power-to-weight ratio of the Huracán Spyder range, thanks to a 35 kg weight reduction and an additional 30 hp compared to the Huracán 4WD Spyder. It uses the Forged Composites® material and the new A.L.A. (Aerodynamic Lamborghini Attiva). Huracán Performante Spyder: Power has gotten beautiful.",
     brandId: 3,
   },
   {
-    name: "Lambo Urus Pearl",
-    slug: "Lambo-Urus-Pearl",
+    name: "Lamborghini Urus Pearl",
+    slug: "Lamborghini-Urus-Pearl",
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Lambo-Urus-Pearl.jpg",
-    price: 517826,
-    description: "Luxury I love It C:",
+    price: 239231,
+    description:
+      "The Urus Pearl capsule collection radiates the typical vivid colors of the Lamborghini tradition, with a unique pearl paint for new customization possibilities, without sacrificing the extreme proportions and chilling performance of the world's first Super-SUV. Freedom in its purest form is taken to another level: you will find new ways to express yourself while exploring new routes and all kinds of terrain. Conquer the road, unleash your personality.",
     brandId: 3,
   },
   {
-    name: "Lambo Urus Graphite",
-    slug: "Lambo-Urus-Graphite",
+    name: "Lamborghini Urus Graphite",
+    slug: "Lamborghini-Urus-Graphite",
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Lambo_Urus_Graphite.jpg",
-    price: 517826,
-    description: "Luxury I love It C:",
+    price: 254332,
+    description: `The new Urus Graphite Capsule sports range gives us all the possibilities to go where we want, with an inimitable character and style. The power and dynamism of the exclusive configurations, which combine the new and sophisticated matt satin-effect tones with the intense colors of the details, open the way to new and seductive interpretations of our personality.
+    Now is the time to express yourself to the fullest and venture into new challenges, choosing the way to reveal our most authentic selves aboard the world's first Super SUV. Unlock any path, unlock your personality.`,
     brandId: 3,
   },
   {
-    name: "Lambo Asterion",
-    slug: "Lambo-Asterion",
+    name: "Lamborghini Asterion",
+    slug: "Lamborghini-Asterion",
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Lambo-Asterion-01.jpg",
-    price: 517826,
-    description: "Luxury I love It C:",
+    price: 200000,
+    description: `The Lamborghini Asterion is a one-of-a-kind car, exploring completely new and unknown territories with a sinuous and sensual design. Luxury and elegance take center stage in the Asterion's driving experience, as evidenced by the use of special materials such as aluminum, forged carbon fiber and titanium.`,
     brandId: 3,
   },
   {
-    name: "Lambo Hurcan Evo Fluo Capsule",
-    slug: "Lambo-Hurcan-Evo-Fluo-Capsule",
+    name: "Lamborghini Hurcan Evo Fluo Capsule",
+    slug: "Lamborghini-Hurcan-Evo-Fluo-Capsule",
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Lambo_Hurcan_Evo_Fluo_Capsule.jpg",
-    price: 517826,
-    description: "Luxury I love It C:",
+    price: 260000,
+    description: `The Huracán EVO Fluo Capsule features exclusive shades that open up new possibilities, awaken dreams and make fantasies fly. Five different interior and exterior configurations combine matte elements with bright, bold colors that will make this new color palette highlight your personality and individual style like never before.
+    A very sporty style, a daring attitude and a touch of glamor: it all comes together perfectly in this sublime masterpiece.
+    Do you want to make the road yours?`,
     brandId: 3,
   },
   {
@@ -60,7 +70,8 @@ module.exports = [
     image:
       "https://dhqdctvzoxoazqvdwpwj.supabase.in/storage/v1/object/public/public-bucket/assets/Lambo-Aventador.webp",
     price: 517826,
-    description: "Luxury I love It C:",
+    description: `Imagine a supercar in which the most advanced technological solutions and the inimitable Aventador design blend with the thrills of open-air driving. The result is an experience of pure adrenaline.
+    With a production of just 250 units, the Aventador 780-4 Ultimae Roadster is the latest open top version of a legendary family. Its 780 hp V12 engine, the most powerful in the history of Lamborghini and the last aspirated as standard, makes this model the most iconic in the range.`,
     brandId: 3,
   },
 ];
