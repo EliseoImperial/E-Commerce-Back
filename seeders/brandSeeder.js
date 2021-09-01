4a354081cca35016d169512a6831a2fe16a415d7
iAgis <86081166+iAgis@users.noreply.github.com>
2021-09-01 20:39:40 -0300

    Update brandSeeder.js

--- seeders/brandSeeder.js
+++ seeders/brandSeeder.js
@@ -6,20 +6,18 @@ module.exports = async () => {
   brands.push(
     {
       name: "Tesla",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/1587px-Tesla_Motors.svg.png",
       description:
         "Tesla, Inc. is an electric vehicle and clean energy company based in Palo Alto, California, United States. Tesla designs and manufactures electric cars, battery energy storage from home to grid-scale, solar panels and solar roof tiles, and related products and services.",
     },
     {
       name: "Ferrari",
-      logo: "https://www.carlogos.org/car-logos/ferrari-logo-750x1100.png",
       description:
         "Ferrari S.p.A. is an Italian luxury sports car manufacturer based in Maranello, Italy. Founded by Enzo Ferrari in 1939 out of the Alfa Romeo race division as Auto Avio Costruzioni, the company built its first car in 1940, and produced its first Ferrari-badged car in 1947.",
     },
     {
       name: "Lamborghini",
-      logo: "https://w7.pngwing.com/pngs/259/599/png-transparent-lamborghini-logo-lamborghini-sports-car-audi-logo-lamborghini-emblem-car-gold.png",
-      description: "Automobili Lamborghini S.p.A. is an Italian brand and manufacturer of luxury sports cars and SUVs based in Sant'Agata Bolognese. The company is owned by the Volkswagen Group through its subsidiary Audi.",
+      description:
+        "Automobili Lamborghini S.p.A. is an Italian brand and manufacturer of luxury sports cars and SUVs based in Sant'Agata Bolognese. The company is owned by the Volkswagen Group through its subsidiary Audi.",
     }
   );
 
