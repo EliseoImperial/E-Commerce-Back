5dcc3fd28c05bc9e8c2b85066478a517d3187be2
iAgis <86081166+iAgis@users.noreply.github.com>
2021-09-16 21:44:48 -0300

    Update validUser.js

--- utils/user/validUser.js
+++ utils/user/validUser.js
@@ -1,6 +1,6 @@
 module.exports = (user) => {
   return (
-    user.email.match(/([a-zA-Z0-9]{3})/i) !== null // Falta validar email correctamente.
+    user.email.match(/([a-zA-Z0-9]{1})/i) !== null // Falta validar email correctamente.
     // Falta validar el resto de las cosas.
   );
 };
