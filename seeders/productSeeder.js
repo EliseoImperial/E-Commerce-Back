0a860edb65b962a7aa77e981e24d77335959f539
Agustin Lemes <86081166+iAgis@users.noreply.github.com>
2022-04-02 20:10:45 -0300

    correct tesla img path & comment others routes

--- seeders/productSeeder.js
+++ seeders/productSeeder.js
@@ -4,7 +4,8 @@ const ferrariSeeder = require("./ferrariSeeder");
 const lamborghiniSeeder = require("./lamborghiniSeeder");
 
 module.exports = async () => {
-  const products = [...teslaSeeder, ...ferrariSeeder, ...lamborghiniSeeder];
+  // const products = [...teslaSeeder, ...ferrariSeeder, ...lamborghiniSeeder];
+  const products = [...teslaSeeder]; //, ...ferrariSeeder, ...lamborghiniSeeder];
   await Product.bulkCreate(products);
   console.log("[Database] Se corrió el seeder de Productos.");
 };
