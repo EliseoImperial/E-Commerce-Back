313e506035a54fead1aaef876c6b7be36ecbeb50
Agustin Lemes <86081166+iAgis@users.noreply.github.com>
2023-08-03 23:07:29 -0300

    upd supabase

--- seeders/teslaSeeder.js
+++ seeders/teslaSeeder.js
@@ -3,7 +3,7 @@ module.exports = [
     name: "Tesla M3",
     slug: "Tesla-M3",
     image:
-      "https://qkriyieyzqvnrddgqmux.supabase.co/storage/v1/object/public/imgs/T-m3.jpg",
+      "https://phlxuexrdqzijdkoxvbe.supabase.co/storage/v1/object/public/imgs/T-m3.jpg",
     price: 48490,
     description:
       "Model 3 comes with the option of dual motor all-wheel drive, 20” Überturbine Wheels and Performance Brakes and lowered suspension for total control, in all weather conditions. And a carbon fiber spoiler improves stability at high speeds, all allowing Model 3 to accelerate from 0-60 mph* in as little as 3.1 seconds.",
@@ -14,7 +14,7 @@ module.exports = [
     name: "Tesla MS",
     slug: "Tesla-MS",
     image:
-      "https://qkriyieyzqvnrddgqmux.supabase.co/storage/v1/object/public/imgs/T-ms.jpg",
+      "https://phlxuexrdqzijdkoxvbe.supabase.co/storage/v1/object/public/imgs/T-ms.jpg",
     price: 91900,
     description:
       "With the longest range and quickest acceleration of any electric vehicle in production, Model S Plaid is the highest performing sedan ever built. Both Long Range and Plaid powertrains, with updated battery architecture, are capable of back-to-back, consistent 1/4 mile runs.",
@@ -25,7 +25,7 @@ module.exports = [
     name: "Tesla MX",
     slug: "Tesla-MX",
     image:
-      "https://qkriyieyzqvnrddgqmux.supabase.co/storage/v1/object/public/imgs/T-mx.jpg",
+      "https://phlxuexrdqzijdkoxvbe.supabase.co/storage/v1/object/public/imgs/T-mx.jpg",
     price: 119990,
     description:
       "With the most power and quickest acceleration of any SUV, Model X Plaid is the highest performing SUV ever built. Both Long Range and Plaid powertrains, with updated battery architecture, can deliver instant torque at any speed.",
@@ -36,7 +36,7 @@ module.exports = [
     name: "Tesla MY",
     slug: "Tesla-MY",
     image:
-      "https://qkriyieyzqvnrddgqmux.supabase.co/storage/v1/object/public/imgs/T-my.jpg",
+      "https://phlxuexrdqzijdkoxvbe.supabase.co/storage/v1/object/public/imgs/T-my.jpg",
     price: 53190,
     description:
       "Tesla All-Wheel Drive has two ultra-responsive, independent electric motors that digitally control torque to the front and rear wheels—for far better handling, traction and stability control. Model Y is capable in rain, snow, mud and off-road.",
